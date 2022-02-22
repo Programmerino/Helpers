f4fb4dc9c197d81ab45734621bd8b8f4fb6a26c0
Davis Davalos-DeLosh <davis@0bit.dev>
2022-02-21 20:36:23 -0700

    Refactor and programName addition

--- Helpers.fs
+++ Helpers.fs
@@ -4,7 +4,21 @@ open System
 open FSharp.Control.Reactive
 open FSharpPlus
 
-let (=>) x y = (not x) || y
+module Logic =
+    let (=>) x y = (not x) || y
+
+module ArgumentParsing =
+    let programName baseName =
+        match Environment.OSVersion.Platform with
+        | PlatformID.Win32NT
+        | PlatformID.Win32S
+        | PlatformID.Win32Windows
+        | PlatformID.WinCE
+        | PlatformID.Xbox -> baseName ++ ".exe"
+        | PlatformID.Unix
+        | PlatformID.MacOSX
+        | PlatformID.Other
+        | _ -> baseName
 
 module Extensions =
     [<RequireQualifiedAccess>]
@@ -23,40 +37,41 @@ module Extensions =
 
         let assertion (cond: bool) : Unit option = asserttap (konst cond) ()
 
-let log x = printfn $"###{x}"
+    [<RequireQualifiedAccess>]
+    module String =
+        let inline toCSVString x =
+            x
+            |> map (String.replace "," ";")
+            |> String.intercalate ","
 
-let inline expandObservable x =
-    match x with
-    | Some x -> x |> Observable.map Some
-    | None -> Observable.single None
+    [<RequireQualifiedAccess>]
+    module Decimal =
+        let fromPriceString x =
+            x
+            |> String.replace "$" ""
+            |> String.replace "From" ""
+            |> String.replace "," ""
+            |> String.replace " " ""
+            |> Result.protect decimal
+            |> Result.mapError (fun y -> Exception($"{x} ") ++ y)
 
-let inline convertPrice x =
-    x
-    |> String.replace "$" ""
-    |> String.replace "From" ""
-    |> String.replace "," ""
-    |> String.replace " " ""
-    |> Result.protect decimal
-    |> Result.mapError (fun y -> Exception($"{x} ") ++ y)
+    [<RequireQualifiedAccess>]
+    module Observable =
+        let ofOption x =
+            match x with
+            | Some x -> x |> Observable.map Some
+            | None -> Observable.single None
 
-let inline csv x =
-    x
-    |> map (String.replace "," ";")
-    |> String.intercalate ","
+    [<RequireQualifiedAccess>]
+    module Result =
+        let bisequence x =
+            match x with
+            | (Ok x), (Ok y) -> Ok(x, y)
+            | (Error e), (Ok _) -> Error e
+            | (Ok _), (Error e) -> Error e
+            | (Error e), (Error _) -> Error e
 
-let inline optBisequence x =
-    match x with
-    | (Ok x), (Ok y) -> Ok(x, y)
-    | (Error e), (Ok y) -> Error e
-    | (Ok x), (Error e) -> Error e
-    | (Error e), (Error f) -> Error e
-
-let inline expandObservableOpt x =
-    match x with
-    | Some x -> x |> Observable.map Some
-    | None -> Observable.single None
-
-let inline expandObservableRes (x: Result<IObservable<'a>, 'b>) =
+let expandObservableFstRes (x: Result<IObservable<'a>, 'b>) =
     match x with
     | Ok y -> y |> Observable.map Ok
     | Error y -> Observable.single (Error y)
